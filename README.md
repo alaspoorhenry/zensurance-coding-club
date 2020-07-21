464125b2389b269547268c2ab2b5b0f838d3df97
alaspoorhenry <123henryxx@gmail.com>
2020-07-21 15:59:11 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# coding-club-2020-07-20-q-pmtx6a
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/coding-club-2020-07-20-q-pmtx6a)
---
+++ index.ts
@@ -0,0 +1,26 @@
+// Import stylesheets
+import "./style.css";
+
+// Write Javascript code!
+const appDiv = document.getElementById("app");
+appDiv.innerHTML += `<h4>Coding Club 2020-07-20 🎉🎉</h4>`;
+appDiv.innerHTML += `<h3>Question: <br/><br/>Write a function that converts an object into an array of keys and values.</b></h3>`;
+
+/**
+ * @param obj
+ */
+
+// Implement your logic here:
+function objectToArray(obj) {
+	const properties = Object.keys(obj)
+
+  return properties.map((key) => (
+    [key, obj[key]]
+  ))
+}
+
+// objectToArray({
+//   likes: 2,
+//   dislikes: 3,
+//   followers: 10
+// })
